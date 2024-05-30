1d62e0f6fa7ae9f1d8cf175bb870d747bb42bb33
har1kaush1k <harikaushik@gmail.com>
2024-05-30 15:42:21 -0700

    use node sdk to perform search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,48 +1,45 @@
 import { RepositoryData } from "@/lib/types"
+import { Trufflepig } from 'trufflepig-node'
 
 export async function POST(request: Request) {
   if (!process.env.TP_API_KEY) {
     throw new Error("Missing Trufflepig API key: TP_API_KEY")
   }
 
+  const trufflepig = new Trufflepig(process.env.TP_API_KEY)
   const { query } = await request.json()
+
+  const index = await trufflepig.getIndex('github-search-engine')
+  if (!index) {
+    return new Response("Index not found", { status: 404 })
+  }
  
   try {
-    const response = await fetch(`http://api.trufflepig.ai/v0/indexes/github-search-engine-2/search?query_text=${query}&max_results=25`, {
-      method: 'POST',
-      headers: {
-        'x-api-key': process.env.TP_API_KEY
-      }
-    })
-
-    if (!response.ok) {
-      console.error(`Error fetching data: [${response.status}] ${response.statusText}`)
-      return new Response(`Error fetching data: [${response.status}] ${response.statusText}`, { status: response.status })
-    }
-
-    const results = await response.json()
-
+    const { results } = await index.search({ queryText: query, maxResults: 25})
     const repoDataMap = new Map<string, RepositoryData>()
+
+    console.table({ query })
+    console.table(results, ["documentKey", "score"])
 
     for (const result of results) {
       if (
-        result.document_key && 
-        !repoDataMap.has(result.document_key) && 
+        result.documentKey && 
+        !repoDataMap.has(result.documentKey) && 
         result.metadata && result.score > 0.10
       ) {
         const metadata = result.metadata
-        const [, owner, repo] = result.document_key.split('/').slice(-3)
+        const [, owner, repo] = result.documentKey.split('/').slice(-3)
    
         const repoData: RepositoryData = {
           name: owner + '/' + repo,
           description: metadata.description,
           stargazers_count: metadata.stars,
           language: metadata.language,
-          html_url: result.document_key,
+          html_url: result.documentKey,
           score: result.score
         }
 
-        repoDataMap.set(result.document_key, repoData)
+        repoDataMap.set(result.documentKey, repoData)
       }
     }
 
